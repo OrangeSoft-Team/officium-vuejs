e68c24c03f9533e18d0b03a2ee3e93ca89aa88e9
Alan Saul <alansaul25@gmail.com>
2021-06-27 14:56:13 -0400

    Implementa vista de las tablas de las ofertas laborales y del detalle
    Se implementa la vista de ofertas laborales en forma de tabla, se implementa el controlador del detalle de la oferta laboral y se estructura template del detalle de la oferta laboral

--- src/comun/infraestructura/UI/views/OfertasLaborales.vue
+++ src/comun/infraestructura/UI/views/OfertasLaborales.vue
@@ -1,30 +1,111 @@
 <template>
-    <v-container fluid>
+    <v-container fluid class="grey lighten-5">
         <v-progress-circular
             v-show="estaCargando"
             indeterminate
             color="primary"
         ></v-progress-circular>
 
-        <div>
-            <ul>
-                <li v-for="(oferta, i) in ofertasLaborales" :key="i">
-                    {{ oferta.titulo }}
-                </li>
-            </ul>
-        </div>
+        <v-row
+            align="center"
+            no-gutters
+            style="height: 50px;"
+        >
+        </v-row>
+        
+        <v-row
+            align="center"
+            no-gutters
+            style="height: 150px;"
+        >
+            <v-col
+                cols="12"
+                sm="1"
+                md="1"
+                lg="1"
+                xl="1"
+            >   
+            </v-col>
+            <v-col
+                cols="12"
+                sm="10"
+                md="10"
+                lg="10"
+                xl="10"
+            >   
+                <v-row
+                    align="center"
+                    justify="space-around"
+                    style="height: 100px;"
+                >
+                    <h3
+                    class="primary--text"
+                    
+                    >
+                    <strong>Ofertas</strong>
+                    </h3>   
+                    <v-btn
+                    depressed
+                    color="primary"
+                    >
+                    Agregar nueva oferta
+                    </v-btn>
+                </v-row>
+                <v-data-table
+                    :headers="headersTable"
+                    :items="ofertasLaborales"
+                    :items-per-page="10"
+                    class="elevation-1"
+                >
+                    <template v-slot:item="row">
+                        <tr>
+                            <td>{{ row.item.titulo }}</td>
+                            <td>{{ row.item.cargo }}</td>
+                            <td>{{ row.item.fechaPublicacion }}</td>
+                            <td>{{ row.item.numeroVacantes }}</td>
+                            <td>{{ row.item.turnoTrabajo }}</td>
+                            <td>
+                                <v-btn depressed rounded color="primary" fab small
+                                @click="verDetalleOfertaLaboral(row.item.idOfertaLaboral)">
+                                    <v-icon>fas fa-search</v-icon>
+                                </v-btn>
+                            </td>
+                        </tr>
+                    </template>
+                </v-data-table>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="1"
+                md="1"
+                lg="1"
+                xl="1"
+            >   
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { ControladorObtenerOfertasLaboralesActivas } from "../../../../ofertaLaboral/infraestructura/controlador/ControladorObtOfertasLaborales";
+//Importamos la interface del DTO para que el objeto a mostrar en la tabla
+//sea del mismo tipo que el que se trae en la respuesta del CU
+import { OfertaLaboralEmpresaDTO } from "../../../../ofertaLaboral/aplicacion/dto/OfertaLaboralEmpresaDTO";
 
 export default Vue.extend({
     data() {
         return {
             estaCargando: true,
-            ofertasLaborales: {},
+            ofertasLaborales: [] as OfertaLaboralEmpresaDTO[],
+            headersTable: [
+                { text: "Titulo", value: "titulo" },
+                { text: "Cargo", value: "cargo" },
+                { text: "Fecha publicacion", value: "fechaPublicacion" },
+                { text: "Número de vacantes", value: "numeroVacantes" },
+                { text: "Turno de trabajo", value: "turnoTrabajo" },
+                { text: "Acciones", value: "acciones" },
+            ]
         };
     },
 
@@ -51,6 +132,13 @@ export default Vue.extend({
             .catch((e) => {
                 console.error(e);
             });
+    },
+    methods: {
+        verDetalleOfertaLaboral(idOfertaLaboral:number) {
+            //console.log('ID de la oferta laboral: ' + idOfertaLaboral);
+            
+            //Llamamos al componente del detalle de oferta laboral
+        }
     },
 });
 </script>
---
+++ src/ofertaLaboral/infraestructura/controlador/ControladorDetalleOfertaLaboral.ts
@@ -0,0 +1,46 @@
+import { JSONOfertaLaboralRepositorio } from "../../infraestructura/JSON/JSONOfertaLaboral.repositorio";
+import { Resultado } from "../../../comun/dominio/resultado";
+import {
+    ObtenerOfertaLaboral,
+    SolicitudOfertaLaboralDTO,
+} from "../../aplicacion/casoDeUso/ObtenerOfertaLaboralDetalle.cu";
+import { OfertaLaboralEmpresaDTO } from "../../aplicacion/dto/OfertaLaboralEmpresaDTO";
+import { IOfertasLaboralesRepo } from "../../aplicacion/IOfertaLaboral.repositorio";
+
+//Controlador de CU Obtener detalle de Oferta Laboral
+export class ControladorDetalleOfertaLaboral {
+    private RepositorioOfertasLaborales: IOfertasLaboralesRepo;
+    //private CasoUsoObtenerOfertasLaborales: ObtenerOfertasLaboralesActivas;
+
+    private constructor(repoAUsar: IOfertasLaboralesRepo) {
+        this.RepositorioOfertasLaborales = repoAUsar;
+    }
+
+    //Método estático para inicializar controlador
+    public static inicializar(): ControladorDetalleOfertaLaboral {
+        return new ControladorDetalleOfertaLaboral(
+            new JSONOfertaLaboralRepositorio()
+        );
+    }
+
+    public async ejecutarCU(
+        solicitud: SolicitudOfertaLaboralDTO
+    ): Promise<Resultado<OfertaLaboralEmpresaDTO>> {
+        const CasoUsoObtenerOfertaLaboral =
+            new ObtenerOfertaLaboral(
+                this.RepositorioOfertasLaborales
+            );
+
+        const respuestaCU = await CasoUsoObtenerOfertaLaboral.ejecutar(
+            solicitud
+        );
+
+        if (respuestaCU.esExitoso) {
+            return Resultado.ok<OfertaLaboralEmpresaDTO>(
+                respuestaCU.getValue()
+            );
+        } else {
+            return Resultado.falla<any>(respuestaCU.error);
+        }
+    }
+}
