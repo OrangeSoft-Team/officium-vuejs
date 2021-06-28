278e281a4ec5a6a5edad75d50a66f1316d9fbecc
Lucas <lpariza.13@est.ucab.edu.ve>
2021-06-27 21:33:02 -0400

    Implementa crear oferta laboral en el repositorio JSON de OfertaLaboral
    Se agrega datos de prueba en el JSON de respuesta

--- src/ofertaLaboral/infraestructura/JSON/__mocks__/JSONOfertaLaboral.repositorio.ts
+++ src/ofertaLaboral/infraestructura/JSON/__mocks__/JSONOfertaLaboral.repositorio.ts
@@ -4,8 +4,17 @@ import { Resultado } from "../../../../comun/dominio/resultado";
 import { IOfertasLaboralesRepo } from "../../../aplicacion/IOfertaLaboral.repositorio";
 import { OFERTAS_LABORALES_RESPUESTA_VALIDA, OFERTA_LABORAL_RESPUESTA_VALIDA } from "../ofertasLaboralesRespuestas";
 import { SolicitudOfertaLaboralDTO } from "@/ofertaLaboral/aplicacion/casoDeUso/ObtenerOfertaLaboralDetalle.cu";
+import { SolicitudCreacionOfertaLaboralDTO } from "@/ofertaLaboral/aplicacion/casoDeUso/CrearOfertaLaboral.cu";
 
 export class JSONOfertaLaboralRepositorio implements IOfertasLaboralesRepo {
+    crearOfertaLaboral(
+        id: SolicitudCreacionOfertaLaboralDTO
+        ): Resultado<OfertaLaboralEmpresaDTO> {
+            return Resultado.ok<OfertaLaboralEmpresaDTO>(
+                OFERTA_LABORAL_RESPUESTA_VALIDA
+            );
+    }
+
     obtenerOfertasLaboralesActivas(
         id: SolicitudOfertasLaboralesActivasDTO
     ): Resultado<OfertaLaboralEmpresaDTO[]> {
