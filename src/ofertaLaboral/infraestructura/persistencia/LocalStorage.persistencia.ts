94475efe31d925d3b1251dab9edf71541978252a
Miguel <migueldeolim1@gmail.com>
2021-06-30 09:27:28 -0400

    Implementa test de integración sobre Adaptador LocalStorage
    Se desarrolla el test de integración para validar funciones de guardado, recuperación y borrado de los valores de Local Storage

--- src/ofertaLaboral/infraestructura/persistencia/LocalStorage.persistencia.ts
+++ src/ofertaLaboral/infraestructura/persistencia/LocalStorage.persistencia.ts
@@ -3,12 +3,12 @@ import { Resultado } from "../../../comun/dominio/resultado";
 
 export class LocalStoragePersistencia implements IServicioPersistencia {
     obtener<T>(clave: string): Resultado<T> {
-        console.log("[LS] ME SOLICITAN ", clave);
+        //console.log("[LS] ME SOLICITAN ", clave);
         const recuperado = JSON.parse(<string>localStorage.getItem(clave));
         return Resultado.ok<T>(recuperado);
     }
     guardar(clave: string, valor: any): Resultado<any> {
-        console.log("[LS] GUARDO ", clave, valor);
+        //console.log("[LS] GUARDO ", clave, valor);
         localStorage.setItem(clave, JSON.stringify(valor));
         return Resultado.ok<any>(true);
     }
---
+++ src/tests/integracion/comun/persistenciaAlterna.integracion.test.ts
@@ -0,0 +1,92 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { LocalStoragePersistencia } from "../../../ofertaLaboral/infraestructura/persistencia/LocalStorage.persistencia";
+
+describe("Adaptador Persistencia Alterna: Local Storage", () => {
+    let adaptadorLocalStorage: LocalStoragePersistencia;
+
+    const fakeLocalStorage = (function () {
+        let store: { [key: string]: string } = {};
+
+        return {
+            getItem: function (key: string) {
+                return store[key] || null;
+            },
+            setItem: function (key: string, value: any) {
+                store[key] = value.toString();
+            },
+            removeItem: function (key: string) {
+                delete store[key];
+            },
+            clear: function () {
+                store = {};
+            },
+        };
+    })();
+
+    //Claves de test
+    const CLAVE_TEST_ID_EMPRESA = "Test_id_empresa";
+
+    //Valores de prueba
+    const VALOR_TEST_ID_EMPRESA = "55sd5as-ds5ads6";
+
+    beforeAll(() => {
+        adaptadorLocalStorage = new LocalStoragePersistencia();
+
+        Object.defineProperty(window, "localStorage", {
+            value: fakeLocalStorage,
+        });
+    });
+
+    beforeEach(() => {
+        //Limpiamos
+        fakeLocalStorage.clear();
+    });
+
+    it("Guardamos y recuperamos un dato en persistencia", () => {
+        //Guardamos el valor
+        const respuestaGuardarOrError = adaptadorLocalStorage.guardar(
+            CLAVE_TEST_ID_EMPRESA,
+            VALOR_TEST_ID_EMPRESA
+        );
+        expect(respuestaGuardarOrError.esFallido).toBeFalsy();
+        expect(respuestaGuardarOrError.esExitoso).toBeTruthy();
+
+        //Recuperamos
+        const respuestaObtenerOrError = adaptadorLocalStorage.obtener(
+            CLAVE_TEST_ID_EMPRESA
+        );
+        expect(respuestaObtenerOrError.esExitoso).toBeTruthy();
+        expect(respuestaObtenerOrError.esFallido).toBeFalsy();
+
+        //Comparamos
+        expect(respuestaObtenerOrError.getValue()).toBe(VALOR_TEST_ID_EMPRESA);
+    });
+
+    it("Guardamos y borramos un dato en persistencia", () => {
+        //Guardamos el valor
+        const respuestaGuardarOrError = adaptadorLocalStorage.guardar(
+            CLAVE_TEST_ID_EMPRESA,
+            VALOR_TEST_ID_EMPRESA
+        );
+        expect(respuestaGuardarOrError.esFallido).toBeFalsy();
+        expect(respuestaGuardarOrError.esExitoso).toBeTruthy();
+
+        //Boramos
+        const respuestaBorradoOrError = adaptadorLocalStorage.remover(
+            CLAVE_TEST_ID_EMPRESA
+        );
+        expect(respuestaBorradoOrError.esFallido).toBeFalsy();
+        expect(respuestaBorradoOrError.esExitoso).toBeTruthy();
+
+        //Verificamos que no exista
+        const respuestaObtenerOrError = adaptadorLocalStorage.obtener(
+            CLAVE_TEST_ID_EMPRESA
+        );
+        expect(respuestaObtenerOrError.esExitoso).toBeTruthy();
+        expect(respuestaObtenerOrError.esFallido).toBeFalsy();
+        expect(respuestaObtenerOrError.getValue()).toBeNull();
+    });
+});
