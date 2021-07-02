3f52801144ed14d04ffeb93793e74c819b18f84d
Miguel <migueldeolim1@gmail.com>
2021-07-02 09:59:56 -0400

    Implementa mapeador de Datos Basicos de Empresa
    Se desarrolla componente encargado de realizar la conversión a DTO desde la entidad Empresa o viceversa, para ser utlizados por los Casos de usos

---
+++ src/empresa/aplicacion/dto/DatosBasicosEmpresaDTO.ts
@@ -0,0 +1,10 @@
+export interface DatosBasicosEmpresaDTO {
+    uuuidEmpresa?: string;
+    nombreEmpresa: string;
+    correoElectronico: string;
+    direccionCalle: string;
+    codigoPostal: string;
+    uuidPais: string;
+    uuidEstado: string;
+    uuidCiudad: string;
+}
--- src/empresa/dominio/Empresa.ts
+++ src/empresa/dominio/Empresa.ts
@@ -7,9 +7,9 @@ import { codigoPostal } from "../../comun/dominio/valueObjects/codigoPostal";
 import { NombreEmpresa } from "./valueObjects/nombreEmpresa";
 import { Pais } from "../../comun/dominio/entidades/Pais";
 import { Estado } from "../../comun/dominio/entidades/Estado";
-import { Municipio } from "../../comun/dominio/entidades/Municipio";
+import { Ciudad } from "../../comun/dominio/entidades/Ciudad";
 
-interface EmpresaProps {
+export interface EmpresaProps {
     idEmpresa?: Identificador;
     nombre: NombreEmpresa;
     correoElectronico: Correo;
@@ -17,55 +17,11 @@ interface EmpresaProps {
     codigoPostal: codigoPostal;
     pais: Pais;
     estado: Estado;
-    municipio: Municipio;
+    ciudad: Ciudad;
 }
 
 export class Empresa extends Entidad<EmpresaProps> {
     public static crear(props: EmpresaProps): Resultado<Empresa> {
-        if (props.idEmpresa) {
-            let idOrError = Identificador.crear(props.idEmpresa.valor());
-
-            if (idOrError.esFallido)
-                return Resultado.falla<any>(idOrError.error);
-        }
-
-        let nombreOrError = NombreEmpresa.crear(props.nombre.valor());
-        if (nombreOrError.esFallido)
-            return Resultado.falla<any>(nombreOrError.error);
-
-        let correoOrError = Correo.crear(props.correoElectronico.valor());
-        if (correoOrError.esFallido)
-            return Resultado.falla<any>(correoOrError.error);
-
-        let direccionOrError = DireccionCalle.crear(props.direccionCalle.valor());
-        if (direccionOrError.esFallido)
-            return Resultado.falla<any>(direccionOrError.error);
-
-        let codigoPostalOrError = codigoPostal.crear(
-            props.codigoPostal.valor()
-        );
-        if (codigoPostalOrError.esFallido)
-            return Resultado.falla<any>(codigoPostalOrError.esFallido);
-
-            /*
-        let paisOrError = Pais.crear(
-            props.pais
-        );
-        if (paisOrError.esFallido)
-            return Resultado.falla<any>(paisOrError.esFallido);
-
-        let estadoOrError = codigoPostal.crear(
-            props.estado
-        );
-        if (estadoOrError.esFallido)
-            return Resultado.falla<any>(estadoOrError.esFallido);
-
-        let municipioOrError = codigoPostal.crear(
-            props.municipio
-        );
-        if (municipioOrError.esFallido)
-            return Resultado.falla<any>(municipioOrError.esFallido);
-        */
         return Resultado.ok<Empresa>(new Empresa(props));
     }
 }
