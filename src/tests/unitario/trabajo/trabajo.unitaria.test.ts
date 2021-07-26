e92981a895599f9e16df0f625b78a14ecb97b06c
Miguel <migueldeolim1@gmail.com>
2021-07-25 23:18:30 -0400

    Implementa infraestructura para obtener trabajo
    Se implementa JSON de servicios de obtención de los datos. 
    Se agrega test a caso de uso.

--- src/tests/unitario/trabajo/trabajo.unitaria.test.ts
+++ src/tests/unitario/trabajo/trabajo.unitaria.test.ts
@@ -4,20 +4,31 @@ import { LocalStoragePersistencia } from "../../../comun/infraestructura/persist
 import { JSONTrabajosRepositorio } from "../../../trabajo/infraestructura/JSON/JSONTrabajos.repositorio";
 import { TRABAJOS_EMPRESA_VALIDA } from "../../../trabajo/infraestructura/JSON/respuestas/ListadoTrabajos";
 import { ObtenerTrabajos } from "../../../trabajo/aplicacion/casoDeUso/ObtenerTrabajos.cu";
+import { LISTADO_TRABAJOS_DETALLE } from "../../../trabajo/infraestructura/JSON/respuestas/ListadoDetalleTrabajos";
+import { ObtenerTrabajoDetalle } from "../../../trabajo/aplicacion/casoDeUso/ObtenerTrabajoDetalle.cu";
+import { IServicioPais } from "../../../comun/aplicacion/IServicioPais";
+import { IServicioEstado } from "../../../comun/aplicacion/IServicioEstado";
+import { IServicioCiudad } from "../../../comun/aplicacion/IServicioCiudad";
+import { JSONPaisServicio } from "../../../comun/infraestructura/JSON/JSONPais.servicio";
+import { JSONEstadoServicio } from "../../../comun/infraestructura/JSON/JSONEstado.servicio";
+import { JSONCiudadServicio } from "../../../comun/infraestructura/JSON/JSONCiudad.servicio";
 
 jest.mock("../../../trabajo/infraestructura/JSON/JSONTrabajos.repositorio");
 
+let repoImplementacion: ITrabajoRepo;
+let persistenciaImplemetnacion: IServicioPersistencia;
+let paisImplementacion: IServicioPais;
+let estadoImplementacion: IServicioEstado;
+let ciudadImplementacion: IServicioCiudad;
+
+beforeEach(() => {
+    persistenciaImplemetnacion = new LocalStoragePersistencia();
+    repoImplementacion = new JSONTrabajosRepositorio(
+        persistenciaImplemetnacion
+    );
+});
+
 describe("Obtener listado de trabajos de la empresa", () => {
-    let repoImplementacion: ITrabajoRepo;
-    let persistenciaImplemetnacion: IServicioPersistencia;
-
-    beforeEach(() => {
-        persistenciaImplemetnacion = new LocalStoragePersistencia();
-        repoImplementacion = new JSONTrabajosRepositorio(
-            persistenciaImplemetnacion
-        );
-    });
-
     it("Obtiene listado de trabajos validos", () => {
         const DATOS_A_USAR = TRABAJOS_EMPRESA_VALIDA;
 
@@ -32,6 +43,35 @@ describe("Obtener listado de trabajos de la empresa", () => {
             expect(data.esExitoso).toBeTruthy();
             expect(data.esFallido).toBeFalsy();
             expect(data.getValue()).toHaveLength(DATOS_A_USAR.length);
+        });
+    });
+});
+
+describe("Obtener trabajo detallado", () => {
+    const DATO_A_USAR = LISTADO_TRABAJOS_DETALLE[0];
+    paisImplementacion = new JSONPaisServicio();
+    estadoImplementacion = new JSONEstadoServicio();
+    ciudadImplementacion = new JSONCiudadServicio();
+
+    it("Debe obtener el detalle de un trabajo de la empresa", () => {
+        //Inicializamos Caso de Uso
+        const CU = new ObtenerTrabajoDetalle(
+            repoImplementacion,
+            paisImplementacion,
+            estadoImplementacion,
+            ciudadImplementacion
+        );
+        const resultadoCU = CU.ejecutar({
+            uuid_trabajo: <string>DATO_A_USAR.uuid,
+        });
+
+        return resultadoCU.then((data) => {
+            if (data.esFallido) {
+                console.error("[TEST ERROR] ", data.error);
+            }
+            expect(data.esExitoso).toBeTruthy();
+            expect(data.esFallido).toBeFalsy();
+            expect(data.getValue().uuid).toBe(DATO_A_USAR.uuid);
         });
     });
 });
--- src/trabajo/infraestructura/JSON/JSONTrabajos.repositorio.ts
+++ src/trabajo/infraestructura/JSON/JSONTrabajos.repositorio.ts
@@ -4,12 +4,28 @@ import { ITrabajoRepo } from "../../aplicacion/ITrabajos.repositorio";
 import { IServicioPersistencia } from "../../../comun/aplicacion/IServicioPersistencia";
 import { CLAVE_TRABAJOS_EMPRESA } from "../../../comun/infraestructura/persistencia/ClavesLocalStorage";
 import { TRABAJOS_EMPRESA_VALIDA } from "./respuestas/ListadoTrabajos";
+import { SolicitudTrabajoDTO } from "../../aplicacion/casoDeUso/ObtenerTrabajoDetalle.cu";
+import { LISTADO_TRABAJOS_DETALLE } from "./respuestas/ListadoDetalleTrabajos";
+import { OPERACION_FALLIDA } from "../../../comun/aplicacion/dto.respuestaOperaciones/OperacionFallida";
 
 export class JSONTrabajosRepositorio implements ITrabajoRepo {
     private persistenciaAlterna: IServicioPersistencia;
 
     constructor(implPersistencia: IServicioPersistencia) {
         this.persistenciaAlterna = implPersistencia;
+    }
+    obtenerDetalleTrabajo(
+        identificador: SolicitudTrabajoDTO
+    ): Resultado<TrabajoEmpresaDTO> {
+        const listadoTrabajos: TrabajoEmpresaDTO[] = LISTADO_TRABAJOS_DETALLE;
+
+        for (let trabajo of listadoTrabajos) {
+            if (identificador.uuid_trabajo == trabajo!.uuid) {
+                return Resultado.ok<TrabajoEmpresaDTO>(trabajo);
+            }
+        }
+
+        return Resultado.falla<any>(OPERACION_FALLIDA);
     }
 
     obtenerTrabajos(): Resultado<TrabajoEmpresaDTO[]> {
