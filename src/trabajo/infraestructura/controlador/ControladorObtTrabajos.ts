ab64949890d6e1ac27ed51248ef5ac1fad3510ed
Alan Saul <alansaul25@gmail.com>
2021-07-26 22:12:21 -0400

    Implementa controlador de obtener trabajos
    Implementación de controlador de obtener la lista de trabajos

---
+++ src/trabajo/infraestructura/controlador/ControladorObtTrabajos.ts
@@ -0,0 +1,63 @@
+import { JSONTrabajosRepositorio } from "../JSON/JSONTrabajos.repositorio";
+import { Resultado } from "../../../comun/dominio/resultado";
+import { ObtenerTrabajos } from "../../aplicacion/casoDeUso/ObtenerTrabajos.cu";
+import { TrabajoEmpresaDTO } from "../../aplicacion/dto/TrabajoEmpresaDTO";
+import { ITrabajoRepo } from "../../aplicacion/ITrabajos.repositorio";
+import { IServicioPersistencia } from "../../../comun/aplicacion/IServicioPersistencia";
+import { LocalStoragePersistencia } from "../../../comun/infraestructura/persistencia/LocalStorage.persistencia";
+import {
+    CLAVE_ID_EMPRESA,
+    CLAVE_SESION_USUARIO,
+} from "../../../comun/infraestructura/persistencia/ClavesLocalStorage";
+import { RespuestaInicioSesionDTO } from "../../../sesion/aplicacion/dto/RespuestaInicioSesionDTO";
+
+//Controlador de CU Obtener Ofertas Laborales Activas
+export class ControladorObtenerTrabajos {
+    private RepositorioTrabajos: ITrabajoRepo;
+    private ServicioPersistenciaLocal: IServicioPersistencia;
+    //private CasoUsoObtenerOfertasLaborales: ObtenerOfertasLaboralesActivas;
+
+    private constructor(
+        repoAUsar: ITrabajoRepo,
+        persistenciaAUsar: IServicioPersistencia
+    ) {
+        this.RepositorioTrabajos = repoAUsar;
+        this.ServicioPersistenciaLocal = persistenciaAUsar;
+    }
+
+    //Método estático para inicializar controlador
+    //Inyecciones de dependencias
+    public static inicializar(): ControladorObtenerTrabajos {
+        const AdaptadorPersistencia = new LocalStoragePersistencia();
+        return new ControladorObtenerTrabajos(
+            new JSONTrabajosRepositorio(AdaptadorPersistencia),
+            AdaptadorPersistencia
+        );
+    }
+
+    public async ejecutarCU(): Promise<Resultado<TrabajoEmpresaDTO[]>> {
+        const CasoUsoObtenerTrabajos =
+            new ObtenerTrabajos(
+                this.RepositorioTrabajos
+            );
+
+        //Obtenemos id empresa del empleador actual
+        const datosEmpleadorOrError =
+            this.ServicioPersistenciaLocal.obtener<RespuestaInicioSesionDTO>(
+                CLAVE_SESION_USUARIO
+            );
+
+        if (datosEmpleadorOrError.esFallido)
+            return Resultado.falla<any>(datosEmpleadorOrError.error);
+
+        const respuestaCU = await CasoUsoObtenerTrabajos.ejecutar();
+
+        if (respuestaCU.esExitoso) {
+            return Resultado.ok<TrabajoEmpresaDTO[]>(
+                respuestaCU.getValue()
+            );
+        } else {
+            return Resultado.falla<any>(respuestaCU.error);
+        }
+    }
+}
