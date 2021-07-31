eb8b11e4c2f4715b2527ca7eca2fbc51e5bdaf60
Lucas <lpariza.13@est.ucab.edu.ve>
2021-07-31 00:02:05 -0400

    Agrega método culminar trabajo
    Se agrega el método culminar trabajo en la interface.

--- src/trabajo/aplicacion/ITrabajos.repositorio.ts
+++ src/trabajo/aplicacion/ITrabajos.repositorio.ts
@@ -2,10 +2,15 @@ import { Resultado } from "../../comun/dominio/resultado";
 import { SolicitudTrabajoDTO } from "./casoDeUso/ObtenerTrabajoDetalle.cu";
 import { TrabajoEmpresaDTO } from "./dto/TrabajoEmpresaDTO";
 
+
 export interface ITrabajoRepo {
     obtenerTrabajos(): Resultado<TrabajoEmpresaDTO[]>;
 
     obtenerDetalleTrabajo(
         identificador: SolicitudTrabajoDTO
     ): Resultado<TrabajoEmpresaDTO>;
+
+    culminaTrabajo(
+        identificador: SolicitudTrabajoDTO
+    ): Resultado<any>;
 }
